0adfd39cc55b03572383319667e75b44ca6bc19c
DigbyTilse <d.tilse@uqconnect.edu.au>
2017-09-19 08:41:20 +1000

    Created and links files - some basic setup. databaseConnect is connecting successfully to the DB

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Digby
+     * Date: 19/09/2017
+     * Time: 8:07 AM
+     */
+
+    include_once "includes/header.inc";
+    require_once "processingDocuments/dbConnect.inc";
+
+    echo "<title>TeachHub</title>
+
+</head>";
+
+    echo "<body>";
+
+    echo "
+    <h1>TeachHub</h1>
+    
+    ";
+
+
+
+
+
+
+    echo "</body></html>";
---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Digby on 19/09/2017.
+ */
+
+(function () {
+
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+
+            console.log("The document state = " + document.readyState);
+
+        }
+    }
+
+
+}());
+
